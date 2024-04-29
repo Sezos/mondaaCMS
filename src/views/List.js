83fb41391786155b43fb0f01738674756a1ec8de
Badral <badral@mail.com>
2024-04-29 11:32:55 +0800

    last

--- src/views/List.js
+++ src/views/List.js
@@ -19,168 +19,149 @@
 // reactstrap components
 import { useEffect, useState } from "react";
 import {
-    Card,
-    CardHeader,
-    Table,
-    Row,
-    Col,
-    Container,
-    Button,
-    CardBody,
+  Card,
+  CardHeader,
+  Table,
+  Row,
+  Col,
+  Container,
+  Button,
+  CardBody,
 } from "reactstrap";
 import ExcelJS from "exceljs";
 import { saveAs } from "file-saver";
 import { Link } from "react-router-dom";
 
 function dynamicSort(property) {
-    var sortOrder = 1;
-    if (property[0] === "-") {
-        sortOrder = -1;
-        property = property.substr(1);
-    }
-    return function (a, b) {
-        var result =
-            a[property] < b[property] ? -1 : a[property] > b[property] ? 1 : 0;
-        return result * sortOrder;
-    };
+  var sortOrder = 1;
+  if (property[0] === "-") {
+    sortOrder = -1;
+    property = property.substr(1);
+  }
+  return function (a, b) {
+    var result =
+      a[property] < b[property] ? -1 : a[property] > b[property] ? 1 : 0;
+    return result * sortOrder;
+  };
 }
 
 const List = (props) => {
-    const { fields, data, name, onClick, isDownload, SubHeader } = props;
-    const [sortBy, setSortBy] = useState(fields[0]);
-    const [datas, setDatas] = useState(data);
+  const { fields, data, name, onClick, isDownload, SubHeader, Footer } = props;
+  const [sortBy, setSortBy] = useState(fields[0]);
+  const [datas, setDatas] = useState(data);
 
-    useEffect(() => {
-        setDatas(data?.sort(dynamicSort(sortBy || fields[0])));
-    }, [sortBy, data, datas, setDatas, fields]);
+  useEffect(() => {
+    setDatas(data?.sort(dynamicSort(sortBy || fields[0])));
+  }, [sortBy, data, datas, setDatas, fields]);
 
-    async function createExcel(headers, rows) {
-        const workbook = new ExcelJS.Workbook();
-        const sheet = workbook.addWorksheet("My Worksheet");
-        sheet.columns = headers;
-        console.log(headers, rows);
-        for (let i = 0; i < rows.length; i++) {
-            sheet.addRow(rows[i]);
-        }
-        return await workbook.xlsx.writeBuffer();
+  async function createExcel(headers, rows) {
+    const workbook = new ExcelJS.Workbook();
+    const sheet = workbook.addWorksheet("My Worksheet");
+    sheet.columns = headers;
+    console.log(headers, rows);
+    for (let i = 0; i < rows.length; i++) {
+      sheet.addRow(rows[i]);
     }
+    return await workbook.xlsx.writeBuffer();
+  }
 
-    const download = async () => {
-        const stream = await createExcel(
-            fields?.map((dat) => {
-                return { header: dat, key: dat };
-            }),
-            datas
-        );
-        saveAs(new Blob([stream]), "lmao.xlsx");
-    };
-
-    return (
-        <>
-            <Container
-                fluid
-                style={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                }}
-            >
-                <Row>
-                    <div className="col">
-                        <Card className="shadow" style={{ minWidth: "1000px" }}>
-                            <CardHeader className="border-0">
-                                <Row>
-                                    <Col md="10">
-                                        <h3 className="mb-0">{name}</h3>
-                                    </Col>
-                                    {isDownload ? (
-                                        <Col>
-                                            <Button onClick={download}>
-                                                Download
-                                            </Button>
-                                        </Col>
-                                    ) : (
-                                        <></>
-                                    )}
-                                </Row>
-                                {SubHeader ? <SubHeader /> : <></>}
-                            </CardHeader>
-                            <CardBody>
-                                <Table>
-                                    <thead className="thead-light">
-                                        <tr>
-                                            {fields?.map((field, idx) => {
-                                                return (
-                                                    <th
-                                                        scope="col"
-                                                        key={idx}
-                                                        onClick={() => {
-                                                            if (
-                                                                sortBy === field
-                                                            ) {
-                                                                setSortBy(
-                                                                    "-" + field
-                                                                );
-                                                            } else
-                                                                setSortBy(
-                                                                    field
-                                                                );
-                                                        }}
-                                                        style={{
-                                                            userSelect: "none",
-                                                            MozUserSelect:
-                                                                "none",
-                                                            WebkitUserSelect:
-                                                                "none",
-                                                            msUserSelect:
-                                                                "none",
-                                                            cursor: "pointer",
-                                                            color:
-                                                                sortBy ===
-                                                                    field ||
-                                                                sortBy ===
-                                                                    "-" + field
-                                                                    ? "black"
-                                                                    : "",
-                                                        }}
-                                                    >
-                                                        {field}
-                                                    </th>
-                                                );
-                                            })}
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {datas?.map((dat, idx) => {
-                                            return (
-                                                <tr
-                                                    key={idx}
-                                                    onClick={() => {
-                                                        onClick(dat);
-                                                    }}
-                                                >
-                                                    {fields?.map(
-                                                        (field, id) => {
-                                                            return (
-                                                                <td key={id}>
-                                                                    {dat[field]}
-                                                                </td>
-                                                            );
-                                                        }
-                                                    )}
-                                                </tr>
-                                            );
-                                        })}
-                                    </tbody>
-                                </Table>
-                            </CardBody>
-                        </Card>
-                    </div>
-                    <Col></Col>
-                </Row>
-            </Container>
-        </>
+  const download = async () => {
+    const stream = await createExcel(
+      fields?.map((dat) => {
+        return { header: dat, key: dat };
+      }),
+      datas,
     );
+    saveAs(new Blob([stream]), "lmao.xlsx");
+  };
+
+  return (
+    <>
+      <Container
+        fluid
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Row>
+          <div className="col">
+            <Card className="shadow" style={{ minWidth: "1000px" }}>
+              <CardHeader className="border-0">
+                <Row>
+                  <Col md="10">
+                    <h3 className="mb-0">{name}</h3>
+                  </Col>
+                  {isDownload ? (
+                    <Col>
+                      <Button onClick={download}>Download</Button>
+                    </Col>
+                  ) : (
+                    <></>
+                  )}
+                </Row>
+                {SubHeader ? <SubHeader /> : <></>}
+              </CardHeader>
+              <CardBody>
+                <Table>
+                  <thead className="thead-light">
+                    <tr>
+                      {fields?.map((field, idx) => {
+                        return (
+                          <th
+                            scope="col"
+                            key={idx}
+                            onClick={() => {
+                              if (sortBy === field) {
+                                setSortBy("-" + field);
+                              } else setSortBy(field);
+                            }}
+                            style={{
+                              userSelect: "none",
+                              MozUserSelect: "none",
+                              WebkitUserSelect: "none",
+                              msUserSelect: "none",
+                              cursor: "pointer",
+                              color:
+                                sortBy === field || sortBy === "-" + field
+                                  ? "black"
+                                  : "",
+                            }}
+                          >
+                            {field}
+                          </th>
+                        );
+                      })}
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {datas?.map((dat, idx) => {
+                      return (
+                        <tr
+                          key={idx}
+                          onClick={() => {
+                            onClick(dat);
+                          }}
+                        >
+                          {fields?.map((field, id) => {
+                            return <td key={id}>{dat[field]}</td>;
+                          })}
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </Table>
+              </CardBody>
+
+              {Footer ? <Footer /> : <></>}
+            </Card>
+          </div>
+          <Col></Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default List;
--- src/views/WorkHour.js
+++ src/views/WorkHour.js
@@ -28,193 +28,225 @@ import { Col, Row } from "reactstrap";
 import Select from "react-select";
 
 const WorkHourScreen = (props) => {
-    const fields = [
-        "date",
-        "project",
-        "employeeId",
-        "name",
-        "rate",
-        "hours",
-        "isGST",
-        "salary",
-    ];
+  const fields = [
+    "date",
+    "project",
+    "employeeId",
+    "name",
+    "rate",
+    "hours",
+    "isGST",
+    "salary",
+  ];
 
-    const [datas, setDatas] = useState([]);
-    //dates
-    const [fromDate, setFromDate] = useState(
-        new Date().toISOString().split("T")[0]
-    );
-    const [toDate, setToDate] = useState(
-        new Date().toISOString().split("T")[0]
-    );
+  const [datas, setDatas] = useState([]);
+  //dates
+  const [fromDate, setFromDate] = useState(
+    new Date().toISOString().split("T")[0],
+  );
+  const [toDate, setToDate] = useState(new Date().toISOString().split("T")[0]);
 
-    const [type, setType] = useState({ value: 1, label: "Each" });
+  const [type, setType] = useState({ value: 1, label: "Each" });
 
-    const [employees, setEmployees] = useState([]);
-    const [employeeId, setEmployeeId] = useState([]);
-    const [projectLocations, setProjectLocations] = useState([]);
-    const [projectLocationId, setProjectLocationId] = useState([]);
+  const [employees, setEmployees] = useState([]);
+  const [employeeId, setEmployeeId] = useState([]);
+  const [projectLocations, setProjectLocations] = useState([]);
+  const [projectLocationId, setProjectLocationId] = useState([]);
+  const [totalSalary, setTotalSalary] = useState(0);
+  const [totalHours, setTotalHours] = useState(0);
 
-    function handleChange(value, _type) {
-        if (_type === 1) {
-            setFromDate(value.split("T")[0]);
-            if (new Date(value) > new Date(toDate)) {
-                setToDate(value.split("T")[0]);
-            }
-        } else {
-            setToDate(value.split("T")[0]);
-            if (new Date(fromDate) > new Date(value)) {
-                setFromDate(value.split("T")[0]);
-            }
-        }
+  function handleChange(value, _type) {
+    if (_type === 1) {
+      setFromDate(value.split("T")[0]);
+      if (new Date(value) > new Date(toDate)) {
+        setToDate(value.split("T")[0]);
+      }
+    } else {
+      setToDate(value.split("T")[0]);
+      if (new Date(fromDate) > new Date(value)) {
+        setFromDate(value.split("T")[0]);
+      }
     }
+  }
 
-    useEffect(() => {
-        fetch();
-    }, [fromDate, toDate, employeeId, projectLocationId, type]);
+  useEffect(() => {
+    fetch();
+  }, [fromDate, toDate, employeeId, projectLocationId, type]);
 
-    useEffect(() => {
-        initFetch();
-    }, []);
+  useEffect(() => {
+    initFetch();
+  }, []);
 
-    const initFetch = async () => {
-        const employs = [{ value: undefined, label: "Any" }].concat(
-            (await services.getUsers())
-                .map((emp) => {
-                    return {
-                        value: emp.id,
-                        label: emp.firstName + " " + emp.lastName,
-                    };
-                })
-                .filter((lol) => lol.value !== null)
-        );
-
-        const locations = [{ value: undefined, label: "Any" }].concat(
-            (await services.getProjectLocations()).map((proj) => {
-                return {
-                    value: proj.id,
-                    label: proj.name,
-                };
-            })
-        );
-
-        setEmployees(employs);
-        setProjectLocations(locations);
-    };
-
-    const fetch = async () => {
-        if (type.value === 1) {
-            const data = await services.getWorkHours(
-                fromDate,
-                toDate,
-                employeeId?.value,
-                projectLocationId?.value
-            );
-            const newData = data.map((dat) => {
-                return {
-                    project: dat.Project.ProjectLocation.name,
-                    employeeId: dat.User.employeeId,
-                    name: dat.User.firstName,
-                    rate: dat.rate,
-                    date: dat.Project.date.split("T")[0],
-                    hours: dat.hours,
-                    isGST: dat.User.isGST ? "Yes" : "No",
-                    salary: `$${(
-                        dat.hours *
-                        dat.rate *
-                        (dat.User.isGST ? 1.1 : 1)
-                    ).toFixed(1)}`,
-                };
-            });
-            setDatas(newData);
-        } else {
-            const data = await services.getWorkHoursTotal(
-                fromDate,
-                toDate,
-                employeeId?.value,
-                projectLocationId?.value
-            );
-            const newData = data.map((dat) => {
-                return {
-                    employeeId: dat.User.employeeId,
-                    name: dat.User.firstName,
-                    rate: dat.rate,
-                    hours: dat.workHours,
-                    isGST: dat.User.isGST ? "Yes" : "No",
-                    salary: `$${dat.salary}`,
-                };
-            });
-            setDatas(newData);
-        }
-    };
-
-    const open = (idx) => {};
-    const SubHeader = () => {
-        return (
-            <Row>
-                <Col md="2" fluid className="ml-3">
-                    <DatePicker
-                        id="example-datepicker"
-                        value={fromDate}
-                        onChange={(v, f) => handleChange(v, 1)}
-                    />
-                </Col>
-                <Col md="2" fluid className="ml-3">
-                    <DatePicker
-                        id="example-datepicker"
-                        value={toDate}
-                        onChange={(v, f) => handleChange(v, 2)}
-                    />
-                </Col>
-                <Col md="2" fluid className="ml-3">
-                    <Select
-                        value={employeeId}
-                        options={employees}
-                        onChange={(value) => {
-                            setEmployeeId(value);
-                        }}
-                    />
-                </Col>
-                <Col md="2" fluid className="ml-3">
-                    <Select
-                        value={projectLocationId}
-                        options={projectLocations}
-                        onChange={(value) => {
-                            setProjectLocationId(value);
-                        }}
-                    />
-                </Col>
-                <Col md="2" fluid className="ml-3">
-                    <Select
-                        value={type}
-                        options={[
-                            { value: 1, label: "Each" },
-                            { value: 2, label: "Total" },
-                        ]}
-                        onChange={(value) => {
-                            setType(value);
-                        }}
-                    />
-                </Col>
-            </Row>
-        );
-    };
-
-    return (
-        <>
-            <Header />
-            <div className="mt--7 w-screen">
-                <List
-                    data={datas}
-                    fields={fields}
-                    name={"Projects"}
-                    onClick={open}
-                    isDownload={true}
-                    SubHeader={SubHeader}
-                />
-            </div>
-        </>
+  const initFetch = async () => {
+    const employs = [{ value: undefined, label: "Any" }].concat(
+      (await services.getUsers())
+        .map((emp) => {
+          return {
+            value: emp.id,
+            label: emp.firstName + " " + emp.lastName,
+          };
+        })
+        .filter((lol) => lol.value !== null),
     );
+
+    const locations = [{ value: undefined, label: "Any" }].concat(
+      (await services.getProjectLocations()).map((proj) => {
+        return {
+          value: proj.id,
+          label: proj.name,
+        };
+      }),
+    );
+
+    setEmployees(employs);
+    setProjectLocations(locations);
+  };
+
+  const fetch = async () => {
+    let newData = [];
+    if (type.value === 1) {
+      const data = await services.getWorkHours(
+        fromDate,
+        toDate,
+        employeeId?.value,
+        projectLocationId?.value,
+      );
+      newData = data.map((dat) => {
+        return {
+          project: dat.Project.ProjectLocation.name,
+          employeeId: dat.User.employeeId,
+          name: dat.User.firstName,
+          rate: dat.rate,
+          date: dat.Project.date.split("T")[0],
+          hours: dat.hours,
+          isGST: dat.User.isGST ? "Yes" : "No",
+          salary: `$${(
+            dat.hours *
+            dat.rate *
+            (dat.User.isGST ? 1.1 : 1)
+          ).toFixed(1)}`,
+        };
+      });
+      setDatas(newData);
+    } else {
+      const data = await services.getWorkHoursTotal(
+        fromDate,
+        toDate,
+        employeeId?.value,
+        projectLocationId?.value,
+      );
+      newData = data.map((dat) => {
+        return {
+          employeeId: dat.User.employeeId,
+          name: dat.User.firstName,
+          rate: dat.rate,
+          hours: dat.workHours,
+          isGST: dat.User.isGST ? "Yes" : "No",
+          salary: `$${dat.salary}`,
+        };
+      });
+      setDatas(newData);
+    }
+    calculateTotal(newData);
+  };
+
+  const calculateTotal = (datas) => {
+    const totalHrs = datas.reduce((partialSum, a) => partialSum + a.hours, 0);
+    const totalSlr = datas.reduce(
+      (partialSum, a) => partialSum + Number(a.salary?.split("$")[1]),
+      0,
+    );
+    setTotalHours(totalHrs);
+    setTotalSalary(totalSlr);
+  };
+
+  const open = (idx) => {};
+
+  const SubHeader = () => {
+    return (
+      <Row>
+        <Col md="2" fluid className="ml-3">
+          <DatePicker
+            id="example-datepicker"
+            value={fromDate}
+            onChange={(v, f) => handleChange(v, 1)}
+          />
+        </Col>
+        <Col md="2" fluid className="ml-3">
+          <DatePicker
+            id="example-datepicker"
+            value={toDate}
+            onChange={(v, f) => handleChange(v, 2)}
+          />
+        </Col>
+        <Col md="2" fluid className="ml-3">
+          <Select
+            value={employeeId}
+            options={employees}
+            onChange={(value) => {
+              setEmployeeId(value);
+            }}
+          />
+        </Col>
+        <Col md="2" fluid className="ml-3">
+          <Select
+            value={projectLocationId}
+            options={projectLocations}
+            onChange={(value) => {
+              setProjectLocationId(value);
+            }}
+          />
+        </Col>
+        <Col md="2" fluid className="ml-3">
+          <Select
+            value={type}
+            options={[
+              { value: 1, label: "Each" },
+              { value: 2, label: "Total" },
+            ]}
+            onChange={(value) => {
+              setType(value);
+            }}
+          />
+        </Col>
+      </Row>
+    );
+  };
+
+  const Footer = () => {
+    if (totalHours === 0) {
+      return <></>;
+    } else
+      return (
+        <div
+          style={{ display: "flex", justifyContent: "end", marginRight: 20 }}
+        >
+          <div>
+            <b>Total Hours: {totalHours}</b>
+          </div>
+          <div style={{ marginLeft: 20 }}>
+            <b>Total Salary: ${totalSalary.toFixed(2)}</b>
+          </div>
+        </div>
+      );
+  };
+
+  return (
+    <>
+      <Header />
+      <div className="mt--7 w-screen">
+        <List
+          data={datas}
+          fields={fields}
+          name={"Projects"}
+          onClick={open}
+          isDownload={true}
+          SubHeader={SubHeader}
+          Footer={Footer}
+        />
+      </div>
+    </>
+  );
 };
 
 export default WorkHourScreen;
